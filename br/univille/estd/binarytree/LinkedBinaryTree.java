5bc957ee5e0370041cd2cf0d5238d3d7257743dd
CharlesTuan <charles.madureira@univille.br>
2020-09-21 20:00:49 -0300

    arquivos iniciais

---
+++ br/univille/estd/binarytree/LinkedBinaryTree.java
@@ -0,0 +1,148 @@
+package br.univille.estd.binarytree;
+
+/**
+ * Implementacao de uma árvore binária usando estrutura encadeada
+ * @author leandersonandre
+ *
+ * @param <E>
+ */
+public class LinkedBinaryTree<E> {
+	
+	protected BTPosition<E> root; // Referencia para a raiz
+	protected int size;           // Numero de nodos
+	
+	/**
+	 * Construtor de uma árvore vazia
+	 */
+	public LinkedBinaryTree() {
+		root = null; // inicia com uma árvore vazia
+		size = 0;
+	}
+	
+	/**
+	 * Retorna o numero de nodos da árvore
+	 * @return
+	 */
+	public int size() {
+		return -1;
+	}
+	
+	/**
+	 * Retorna se um nodo é interno
+	 */
+	public boolean isInternal(BTPosition<E> v) throws InvalidPositionException{
+		return false;
+	}
+	
+	
+	/**
+	 * Retorna se um nodo é externo
+	 */
+	public boolean isExternal(BTPosition<E> v) throws InvalidPositionException{
+		return false;
+	}
+	
+	/**
+	 * Retorna se um nodo é a raiz
+	 */
+	public boolean isRoot(BTPosition<E> v) throws InvalidPositionException{
+		return false;
+	}
+	/**
+	 * Retorna se um nodo tem o filho da esquerda
+	 */
+	public boolean hasLeft(BTPosition<E> v) throws InvalidPositionException{
+		return false;
+	}
+	
+	/**
+	 * Retorna se um nodo tem o filho da direita
+	 */
+	public boolean hasRight(BTPosition<E> v) throws InvalidPositionException{
+		return false;
+	}
+	
+	/**
+	 * Retorna a raiz da árvore
+	 */
+	public BTPosition<E> root() throws EmptyTreeException{
+		return null;
+	}
+	
+	/**
+	 * Retorna o filho da esquerda de um nodo
+	 * Lança BoundaryViolationException se nao tiver filho da esquerda
+	 */
+	public BTPosition<E> left(BTPosition<E> v) throws InvalidPositionException, BoundaryViolationException{
+		return null;
+	}
+	
+	/**
+	 * Retorna o filho da direita de um nodo
+	 * Lança BoundaryViolationException se nao tiver filho da direita
+	 */
+	public BTPosition<E> right(BTPosition<E> v) throws InvalidPositionException, BoundaryViolationException{
+		return null;
+	}
+	
+	/**
+	 * Retorna o pai de um nodo
+	 * Lança BoundaryViolationException se nao tiver pai
+	 */
+	public BTPosition<E> parent(BTPosition<E> v) throws InvalidPositionException, BoundaryViolationException{
+		return null;
+	}
+	
+	/**
+	 * Substitui o elemento armazenado no nodo
+	 * Retorna o elemento substituido
+	 */
+	public E replace(BTPosition<E> v, E o) throws InvalidPositionException{
+		return null;
+	}
+	
+	/**
+	 * Retorna o irmao de um nodo
+	 * Lança BoundaryViolationException se nao tiver um irmao
+	 */
+	public BTPosition<E> sibling(BTPosition<E> v) throws InvalidPositionException, BoundaryViolationException{
+		return null;
+	}
+	
+	/**
+	 * Insere a raiz em uma arvore vazia
+	 */
+	public BTPosition<E> addRoot(E e) throws NonEmptyTreeException{
+		return null;
+	}
+	
+	/**
+	 * Insere o filho da esquerda em um nodo
+	 */
+	public BTPosition<E> insertLeft(BTPosition<E> v, E e) throws InvalidPositionException{
+		return null;
+	}
+	
+	/**
+	 * Insere o filho da direita em um nodo
+	 */
+	public BTPosition<E> insertRight(BTPosition<E> v, E e) throws InvalidPositionException{
+		return null;
+	}
+	
+	/**
+	 * Remove um nodo com zero ou um filho
+	 * Nao pode remover um nodo com dois filhos. Deve lancar InvalidPositionException
+	 */
+	public E remove(BTPosition<E> v) throws InvalidPositionException{
+		return null;
+	}
+	
+	/**
+	 *  Cria um novo nodo para a arvore binaria
+	 */
+	protected BTPosition<E> createNode(E element, BTPosition<E> parent, BTPosition<E> left, BTPosition<E> right) {
+		return null;
+	}
+
+}
